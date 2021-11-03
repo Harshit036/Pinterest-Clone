72903a7323502e74e262862da8e715606c9ee6a9
devu <80763211+Devanshu236@users.noreply.github.com>
2021-11-03 12:52:38 +0530

    hover to the image addded

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 import Pins from "./components/Pins";
 import Navbar from "./components/Navbar";
-import PicturecardTemp from "./components/PicturecardTemp";
+// import PicturecardTemp from "./components/PicturecardTemp";
 
 function App() {
   const [InputKey, setInputKey] = useState("random");
 
   return (
     <>
-      <PicturecardTemp></PicturecardTemp>
-      {/* <Navbar onSubmit={setInputKey} />
-      <Pins InputKey={InputKey} /> */}
+     
+      <Navbar onSubmit={setInputKey} />
+      <Pins InputKey={InputKey} /> 
     </>
   );
 }
